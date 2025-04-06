14f33280e69e7b9817e87da41a14ce63481d42be
lucasilmor <lucassilmor0@hotmail.com>
2025-04-06 17:02:25 -0300

    teste de conexao com o banco de dados ok

---
+++ edulinker/src/app/api/teste-db/route.ts
@@ -0,0 +1,12 @@
+import { connectToDB } from "@/lib/mongodb"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+    try {
+      await connectToDB()
+      return NextResponse.json({ ok: true, message: 'Conexão bem-sucedida com o MongoDB Atlas' })
+    } catch (error) {
+      console.error(error)
+      return NextResponse.json({ ok: false, message: 'Erro ao conectar com o MongoDB Atlas' }, { status: 500 })
+    }
+  }
---
+++ edulinker/src/lib/mongodb.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const MONGODB_URI = process.env.MONGODB_URI!
+
+if (!MONGODB_URI) {
+  throw new Error('Variável de ambiente MONGODB_URI não está definida.')
+}
+
+let cached = (global as any).mongoose || { conn: null, promise: null }
+
+export async function connectToDB() {
+  if (cached.conn) return cached.conn
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: 'EduLinker',
+      bufferCommands: false,
+    })
+  }
+
+  cached.conn = await cached.promise
+  return cached.conn
+}
